c469e41aadd3f640a30f7ddb6c75ee2562a5ca88
Vitor Soares <vitorsoaresf@gmail.com>
2024-03-19 08:46:50 -0300

    feat: implements conditional operation to view messages from wrong fields

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {
   ReactiveFormsModule,
@@ -9,21 +10,24 @@ import {
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.css',
 })
 export class RegisterFormComponent {
   registerForm = new FormGroup({
-    name: new FormControl(null, [Validators.required, Validators.min(3)]),
+    name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
     email: new FormControl(null, [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.min(8)]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
   });
 
   onSubmit() {
     if (this.registerForm.status === 'VALID') {
       console.log(this.registerForm.value);
-      this.registerForm.reset({ password: '' });
+      this.registerForm.reset();
     } else {
       alert('Dados informados inválido, preencha os campos corretamente');
     }
